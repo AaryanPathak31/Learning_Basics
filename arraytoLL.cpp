9e2c815b668557fde97877e2f1869bc2e2da38cf
Aaryan Pathak <aaryan.pathak3104@gmial.com>
2025-01-06 10:12:19 +0530

    arr2llp

--- arraytoLL.cpp
+++ arraytoLL.cpp
@@ -1,38 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node {
+public:
     int data;
     Node* next;
 
-    public:
-    Node(int data1,Node*next1){
-        data=data1;
-        next=next1;
+    // Constructor to initialize data and link to another node
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
     }
-    public:
-    Node(int data1){
-        data=data1;
-        next=nullptr;
+
+    // Constructor to initialize data and set next to nullptr
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
     }
 };
 
-Node* convertArr2LL(vector<int> &arr){
-    Node* head= new Node(arr[0]);
-    Node* mover = head;
-    for(int i=1;i<arr.size();i++){
-        Node* temp=new Node(arr[i]);
-        mover->next=temp;
-        mover=temp;
+// Function to convert a vector to a linked list
+Node* convertArr2LL(vector<int>& arr) {
+    if (arr.empty()) return nullptr; // Handle empty array
 
+    Node* head = new Node(arr[0]);  // Create the head node
+    Node* mover = head;             // Pointer to build the list
+
+    // Loop through the rest of the array to create nodes
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;  // Link the new node to the list
+        mover = temp;        // Move to the new node
     }
-    return head;
-
+    return head; // Return the head of the linked list
 }
 
-int main(){
-    vector<int>arr = {2,5,7,8};
-    Node* head= convertArr2LL(arr);
-    cout<<head->data;
+// Function to print the entire linked list
+void printLinkedList(Node* head) {
+    Node* current = head;  // Start from the head node
+    while (current != nullptr) {
+        cout << current->data << " "; // Print data of the current node
+        current = current->next;      // Move to the next node
+    }
+    cout << endl; // Add a newline after printing the list
+}
+
+int main() {
+    vector<int> arr = {2, 5, 7, 8};      // Array to be converted to a linked list
+    Node* head = convertArr2LL(arr);    // Convert array to linked list
+    printLinkedList(head);              // Print the entire linked list
+    return 0;
 }
